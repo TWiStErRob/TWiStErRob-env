8f7e3326fc2a7f4f119213235fcbe33d27ba145d
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2021-10-21 22:17:41 +0100

    Change run.bat to get a revision number range.

--- svn2git-migration/run-all.bat
+++ svn2git-migration/run-all.bat
@@ -1,6 +1,7 @@
 @rem Run each in parallel in new windows.
-start "BLT" cmd /K "run.bat monorepo-split-net.twisterrob.travel.rules -travel"
-start "Cinema" cmd /K "run.bat monorepo-split-net.twisterrob.cinema.rules -cinema"
-start "Inventory" cmd /K "run.bat monorepo-split-net.twisterrob.inventory.rules -inventory"
-start "Others" cmd /K "run.bat monorepo-split-independent_projects.rules -all"
+start "BLT" cmd /K "run.bat monorepo-split-net.twisterrob.travel.rules -travel 1 10000"
+start "Cinema" cmd /K "run.bat monorepo-split-net.twisterrob.cinema.rules -cinema 1 10000"
+start "Inventory" cmd /K "run.bat monorepo-split-net.twisterrob.inventory.rules -inventory 1 10000"
+start "Others" cmd /K "run.bat monorepo-split-independent_projects.rules -all 1 10000"
+start "Web/Tests" cmd /K "run.bat net.twisterrob.healthcheck.rules -hc 2645 2656"
 @rem They'll stay open when finished, but the docker container will stop.
--- svn2git-migration/run.bat
+++ svn2git-migration/run.bat
@@ -1,5 +1,7 @@
 @rem %1 = rule file to execute from conf, must exist
-@rem %2 = workdir suffix, can be omitted (and then it'll be empty)
+@rem %2 = workdir suffix
+@rem %3 = min revision
+@rem %4 = max revision
 set SVN_REPO=p:\repos\svn
 set SVN_REPO_SLASH=c:/repos/svn
-docker run --rm -it -v %CD%\conf:/tmp/conf -v %CD%\workdir%2:/workdir -v %SVN_REPO%:/tmp/svn svn2git-work bash -c "/usr/local/svn2git/svn-all-fast-export --identity-map /tmp/conf/migrate.authors --rules /tmp/conf/%1 --debug-rules --stats --svn-ignore --propcheck --empty-dirs --add-metadata --add-metadata-notes --msg-filter 'sed -z -r -f /tmp/conf/svn-msg-filter.sed' /tmp/svn >svn2git.log 2>&1"
+docker run --rm -it -v %CD%\conf:/tmp/conf -v %CD%\workdir%2:/workdir -v %SVN_REPO%:/tmp/svn svn2git-work bash -c "/usr/local/svn2git/svn-all-fast-export --resume-from %3 --max-rev %4 --identity-map /tmp/conf/migrate.authors --rules /tmp/conf/%1 --debug-rules --stats --svn-ignore --propcheck --empty-dirs --add-metadata --add-metadata-notes --msg-filter 'sed -z -r -f /tmp/conf/svn-msg-filter.sed' /tmp/svn >svn2git.log 2>&1"
