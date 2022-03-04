86901d334be9421e9669506757bea7e92e8cd692
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2022-03-04 09:45:28 +0000

    Split out colorfilter rules

--- svn2git-migration/run-all.bat
+++ svn2git-migration/run-all.bat
@@ -1,6 +1,7 @@
 @rem Run each in parallel in new windows.
 start "BLT" cmd /K "run.bat monorepo-split-net.twisterrob.travel.rules -travel 1 10000"
 start "Cinema" cmd /K "run.bat net.twisterrob.cinema.rules -cinema 1 10000"
+start "ColorFilters" cmd /K "run.bat net.twisterrob.colorfilters.rules -color 1 10000"
 start "Gradle" cmd /K "run.bat net.twisterrob.gradle.rules -gradle 1 10000"
 start "Inventory" cmd /K "run.bat monorepo-split-net.twisterrob.inventory.rules -inventory 1 10000"
 start "Others" cmd /K "run.bat monorepo-split-independent_projects.rules -all 1 10000"
