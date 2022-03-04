dc5fc9b1fddaf79d06bf1fbd0286308b86063018
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2022-03-04 09:33:13 +0000

    Rename cinema rule to match new style

--- svn2git-migration/run-all.bat
+++ svn2git-migration/run-all.bat
@@ -1,6 +1,6 @@
 @rem Run each in parallel in new windows.
 start "BLT" cmd /K "run.bat monorepo-split-net.twisterrob.travel.rules -travel 1 10000"
-start "Cinema" cmd /K "run.bat monorepo-split-net.twisterrob.cinema.rules -cinema 1 10000"
+start "Cinema" cmd /K "run.bat net.twisterrob.cinema.rules -cinema 1 10000"
 start "Inventory" cmd /K "run.bat monorepo-split-net.twisterrob.inventory.rules -inventory 1 10000"
 start "Others" cmd /K "run.bat monorepo-split-independent_projects.rules -all 1 10000"
 start "Web/Tests" cmd /K "run.bat net.twisterrob.healthcheck.rules -hc 2645 2656"
